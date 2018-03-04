d0764dea23a2cc100388388ab727b11628e503e5
Srinivas <Srinivas@Srinivas-PC>
2018-03-04 20:52:35 +0530

    Guru99 GIt Test DemoPro

---
+++ HelloMaven/src/main/java/org/o7planning/tutorial/hellomaven/CheckNumeric.java
@@ -0,0 +1,11 @@
+package org.o7planning.tutorial.hellomaven;
+
+public class CheckNumeric {
+
+	 public static void main(String[] args) {
+	      
+	      System.out.println("Hello maven");
+	 
+	  }
+	 
+}
